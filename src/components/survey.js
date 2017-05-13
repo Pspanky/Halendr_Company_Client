baf77de409ac646eb1fd8b615a67d9b51279e760
watermandrew <Drew.C.Waterman.18@Dartmouth.edu>
2017-05-13 19:41:53 -0400

    survey changes

--- src/components/survey.js
+++ src/components/survey.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 // import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, NavLink, Switch } from 'react-router-dom';
 import '../style.scss';
@@ -8,25 +8,67 @@ class Survey extends Component {
     super(props);
   }
   render() {
-    <form className="survey">
-      <div className="surveyQ">
-        <div className="question_heading">
-        Question 1
-      </div>
-        <input type="radio" value="1" name="q1">1</input>
-        <input type="radio" value="2" name="q1">2</input>
-        <input type="radio" value="3" name="q1">3</input>
-        <input type="radio" value="4" name="q1">4</input>
-      </div>
-      <div className="surveyQ">
-        <div className="question_heading">
-        Question 2
-      </div>
-        <input type="radio" value="1" name="q2">1</input>
-        <input type="radio" value="2" name="q2">2</input>
-        <input type="radio" value="3" name="q2">3</input>
-        <input type="radio" value="4" name="q2">4</input>
-      </div>
-    </form>;
+    return (
+      <form className="survey">
+        <div className="surveyQ">
+          <div className="question_heading">
+            Question 1
+          </div>
+          <div className="answers">
+            <input type="radio" value="1" name="q1" />1
+            <input type="radio" value="2" name="q1" />2
+            <input type="radio" value="3" name="q1" />3
+            <input type="radio" value="4" name="q1" />4
+          </div>
+        </div>
+        <div className="surveyQ">
+          <div className="question_heading">
+            Question 2
+          </div>
+          <div className="answers">
+            <input type="radio" value="1" name="q2" />1
+            <input type="radio" value="2" name="q2" />2
+            <input type="radio" value="3" name="q2" />3
+            <input type="radio" value="4" name="q2" />4
+          </div>
+        </div>
+        <div className="surveyQ">
+          <div className="question_heading">
+            Question 3
+          </div>
+          <div className="answers">
+            <input type="radio" value="1" name="q3" />1
+            <input type="radio" value="2" name="q3" />2
+            <input type="radio" value="3" name="q3" />3
+            <input type="radio" value="4" name="q3" />4
+          </div>
+        </div>
+        <div className="surveyQ">
+          <div className="question_heading">
+            Question 4
+          </div>
+          <div className="answers">
+            <input type="radio" value="1" name="q4" />1
+            <input type="radio" value="2" name="q4" />2
+            <input type="radio" value="3" name="q4" />3
+            <input type="radio" value="4" name="q44" />4
+          </div>
+        </div>
+        <div className="surveyQ">
+          <div className="question_heading">
+            Question 5
+          </div>
+          <div className="answers">
+            <input type="radio" value="1" name="q5" />1
+            <input type="radio" value="2" name="q5" />2
+            <input type="radio" value="3" name="q5" />3
+            <input type="radio" value="4" name="q5" />4
+          </div>
+        </div>
+        <button>Submit</button>
+      </form>
+    );
   }
 }
+
+export default Survey;
